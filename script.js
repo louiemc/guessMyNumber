7db497633cda922c8b09818a474c2e4d2b5c5386
louiemc <clouie2@gmail.com>
2021-06-13 21:49:06 -0400

    updated the condition to be score > 1 and the message updates when the player loses and the score then equals 0

--- script.js
+++ script.js
@@ -30,22 +30,37 @@ document.querySelector(".check").addEventListener("click", function () {
   // guess is greater than the secret number
   else if (guess > secretNumber) {
     // let's add our losing scenario here
-    if (score > 0) {
+    if (score > 1) {
+      // has to be 1 not 0 because even when the score hits 0 the message you lose does not appear
+      // so, if the score is above 0 then the code in this code block will be executed
       document.querySelector(".message").textContent = "📈 Too high!";
       // decrease the score
       score--;
       // display the updated score
       document.querySelector(".score").textContent = score;
     } else {
+      // if the score is 0 then the player will lose
       document.querySelector(".message").textContent = "💥 You lose the game!";
+      // set the score to 0 when you lose
+      document.querySelector(".score").textContent = 0;
     }
   }
   // guess is less than the secret number
   else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "📉 Too low!";
-    // decrease the score
-    score--;
-    // display the updated score
-    document.querySelector(".score").textContent = score;
+    // let's add our losing scenario here
+    if (score > 1) {
+      // has to be 1 not 0 because even when the score hits 0 the message you lose does not appear
+      // so, if the score is above 0 then the code in this code block will be executed
+      document.querySelector(".message").textContent = "📈 Too low!";
+      // decrease the score
+      score--;
+      // display the updated score
+      document.querySelector(".score").textContent = score;
+    } else {
+      // if the score is 0 then the player will lose
+      document.querySelector(".message").textContent = "💥 You lose the game!";
+      // set the score to 0 when you lose
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
