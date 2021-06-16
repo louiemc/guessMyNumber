f478402536d0dba6ed0f159bb52fad02653bbe09
louiemc <clouie2@gmail.com>
2021-06-15 20:53:08 -0400

    made another esle if code block but consolidated the guess > secretNumber and guess < secretNumber; still need to figure out how to determine if guess is too high or low

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highscore;
     }
   }
+  /*
   // guess is greater than the secret number
   else if (guess > secretNumber) {
     // let's add our losing scenario here
@@ -68,6 +69,24 @@ document.querySelector(".check").addEventListener("click", function () {
       // has to be 1 not 0 because even when the score hits 0 the message you lose does not appear
       // so, if the score is above 0 then the code in this code block will be executed
       document.querySelector(".message").textContent = "📈 Too low!";
+      // decrease the score
+      score--;
+      // display the updated score
+      document.querySelector(".score").textContent = score;
+    } else {
+      // if the score is 0 then the player will lose
+      document.querySelector(".message").textContent = "💥 You lose the game!";
+      // set the score to 0 when you lose
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+*/
+
+  // a new else if block for when the guess is different (like consolidating the guess > secretNumber and guess < secretNumber blocks)
+  // when the guess is wrong
+  else if (guess !== secretNumber) {
+    // let's add our losing scenario here
+    if (score > 1) {
       // decrease the score
       score--;
       // display the updated score
